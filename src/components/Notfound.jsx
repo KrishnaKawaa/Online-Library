fb12e0800fe6c2a0a623b1c7a740c322c6b5e03b
Krishna-kawaa <krishnakawa69@gmail.com>
2025-03-01 12:07:21 +0530

    creater 404 page for error links

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -1,7 +1,16 @@
+import './notfound.css';
+import { Link } from 'react-router-dom';
+
+
 export default function Notfound(){
     return(
-        <>
-            New Error Page 404
-        </>
+        <div className='errorContainer'>
+            <img src="https://i.namu.wiki/i/FaRIEh0bO8rL5x0rtBp3yMB8Q72tt_MGKpuHkHqgNaSXKRcXrXeWBh1C7Uhk2UO-TJjm-mH5glCNW6PRWLxLdg.webp" alt="errorImageMonster"/>
+            <div className='errorMessage'>
+            <h1>Oops! Page Not Found</h1>
+            <p>You must have picked the wrong door because I haven't been able to lay my eyes on the page you've been searching for.</p>
+            <Link className='homelink' to='/'> Go to Home Page</Link>
+            </div>
+        </div>
     )
 }
